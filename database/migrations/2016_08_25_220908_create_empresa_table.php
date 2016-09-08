b30795b1351073f1756252f461f5404067878d90
Wisney <wisneymaciel2@hotmail.com>
2016-09-08 18:00:52 -0300

    mudando pra pasta principal local

---
+++ database/migrations/2016_08_25_220908_create_empresa_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('cnpj')->unique();
+            $table->string('email')->unique();
+            $table->integer('nivel')->default(3);
+            $table->string('password');
+            $table->string('telefone');
+            $table->rememberToken()->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('empresa');
+    }
+}
+
