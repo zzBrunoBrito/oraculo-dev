35b64c23533f052946c1306c3758c656cb89e45e
Wisney <wisneymaciel2@hotmail.com>
2016-10-24 21:01:49 -0300

    especif. pra lucas tabela bunitinha

--- resources/views/historicoEquip.blade.php
+++ resources/views/historicoEquip.blade.php
@@ -21,6 +21,36 @@
             echo '<td>'.$e->created_at.'</td>';
         }
         ?>
+
+        <!--
+        Tabela com tuas viadagens de responsivo e tudo mais...
+        E tbm que dê pra clicar nela, e descer um menuzim, que vai ser só outra tabela(s).
+        Manda só o codigo de uma aki mesmo, q eu faço minhas gambiarras pro resto <3 :v
+        obs: Vai ter tbm os 'títulos' em cima,só o nome das colunas, faça legalzim e.e
+        obs2: Gambatte Lucas-kun!
+        obs3: Faça logo, vagabundo! u.u
+        Ex pra tu mandar:
+
+        <table style="width:100%">
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th> 
+        <th>Age</th>
+        </tr>
+        <tr>
+        <td>Jill</td>
+        <td>Smith</td> 
+        <td>50</td>
+        </tr>
+        <tr>
+        <td>Eve</td>
+        <td>Jackson</td> 
+        <td>94</td>
+        </tr>
+        </table>
+        
+        !-->
+
         </table>
 
         </div>
