8c0a4861d2edff6c9b3c1243103e90eba5d4592c
Wisney <wisneymaciel2@hotmail.com>
2017-01-29 05:29:31 -0300

    mais 1 na lista de 'coisas para fazer'

--- resources/views/auth/registerequip.blade.php
+++ resources/views/auth/registerequip.blade.php
@@ -241,7 +241,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i> Registrar
+                                    <i class="fa fa-btn fa-gears"></i> Registrar
                                 </button>
                             </div>
                         </div>
